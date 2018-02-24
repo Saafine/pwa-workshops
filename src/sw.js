8ae80cf0d0f4f1289f2d6f0de9a887a2e3191987
Pawel Labus <pawel.labus@xtb.com>
2018-02-24 14:19:10 +0100

    [+] prod test

--- src/sw.js
+++ src/sw.js
@@ -39,22 +39,24 @@ self.addEventListener('activate', (event) => {
  * - fetch other requests and cache them if needed
  */
 self.addEventListener('fetch', function(evt) {
-console.log('The service worker is serving the asset.');
-evt.respondWith(fromCache(evt.request));
-evt.waitUntil(update(evt.request));
+  // console.log('The service worker is serving the asset.');
+  evt.respondWith(fromCache(evt.request));
+  evt.waitUntil(update(evt.request));
 });
 
 function fromCache(request) {
-  return caches.open(CACHE_NAME).then(function (cache) {
-    return cache.match(request).then(function (matching) {
+  return caches.open(CACHE_NAME).then(function(cache) {
+    return cache.match(request).then(function(matching) {
       return matching || Promise.reject('no-match');
+    }).catch((error) => {
+      // console.log(error);
     });
   });
 }
 
 function update(request) {
-  return caches.open(CACHE_NAME).then(function (cache) {
-    return fetch(request).then(function (response) {
+  return caches.open(CACHE_NAME).then(function(cache) {
+    return fetch(request).then(function(response) {
       return cache.put(request, response);
     });
   });
